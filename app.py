4d03578f2d9f6183993e003301b74c0d3bf7d104
makoto@Makotos-MacBook.local <makoto@Makotos-MacBook.local.local>
2022-08-13 15:13:33 +0900

    Completed initial development
    added gunicorn
    
    changed title
    
    added LICENSE.txt

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template, request
+from utils.crawl_hr_news import crawl_hr_news
+from datetime import datetime, timedelta
+import os
+
+DEFAULT_TARGET_DATE: str = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
+DEFAULT_FI_WORDS = ['銀行', 'バンク', '金庫', '信金', '信用', '組合', 'ファイナンシャル', 'フィナンシャル', 'ファイナンス', '証券', '證券', '保険', '損保', '生命', '生保', '金融庁']
+
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods = ["GET" , "POST"]) #2 GETとPOSTをリクエストできるようにする
 def index():
-    return 'hello, world'
+    if request.method == 'POST': #3POSTの処理
+      target_date = request.form['target_date']
+      fi_words = request.form['fi_words']
+      result = crawl_hr_news(str(int(target_date.split('-')[-1])), fi_words.split(','))
+      return render_template('index.html', target_date=target_date, fi_words=fi_words, result='\n'.join(result), result_count=len(result) )
+    return render_template('index.html', target_date=DEFAULT_TARGET_DATE, fi_words=','.join(DEFAULT_FI_WORDS)) #6 GETの処理
 
 if __name__ == '__main__':
-    app.run()
+    if os.getenv('FLASK_APP_ENV') == 'PRODUCTION':
+        app.run()
+    else:
+        app.config['TEMPLATES_AUTO_RELOAD'] = True
+        app.jinja_env.auto_reload = True
 
---
+++ utils/crawl_hr_news.py
@@ -0,0 +1,66 @@
+
+import requests
+from bs4 import BeautifulSoup
+import lxml.html
+import time
+import datetime
+import random
+
+def is_target_date(target_date: str, content_date: str):
+    return '日' in content_date and target_date == content_date[0:len(target_date)]
+
+# def is_GRCS_related(article: str):
+#     return any(grcs_word in article for grcs_word in GRCS_WORDS)
+
+BASE_URL: str = 'https://www.nikkei.com'
+MAX_REQUESTS = 1000
+
+TITLE_SELECTOR = '#CONTENTS_MAIN > section > ul:nth-of-type({}) > li:nth-of-type({}) > div > div.col.headline > h3 > a'
+    ##CONTENTS_MAIN > section > ul:nth-child(2) > li:nth-child(1) > div > div.col.headline > h3 > a
+CONTENT_DATE_SELECTOR = '#CONTENTS_MAIN > section > ul:nth-of-type({}) > li:nth-of-type({}) > div > div.col.time > p'
+    ##CONTENTS_MAIN > section > ul:nth-child(2) > li:nth-child(1) > div > div.col.time > p
+HR_CONTENT_XPATH = '/html/body/div[5]/main/article/section[1]'
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+}
+session = requests.Session()
+
+def crawl_hr_news(TARGET_DATE: str, FI_WORDS: list) -> list:
+    stage = 1  # stage: 1-TARGET_DATEに達していない状態, 2-達した状態
+    result = []
+    cnt = 0  # cnt: ループの回数を記録する
+    for bn in range(1, 1982, 30):  # ページのURLが /?bn=31, /?bn=61のように30刻みとなっていることに対応
+        time.sleep(random.randint(1, 2))
+        soup = BeautifulSoup(session.get(BASE_URL+'/news/jinji/hatsurei/?bn='+str(bn), headers=HEADERS).text, features="lxml")
+        # print('[{}] Proceeding with Page-{}'.format(datetime.datetime.now(), bn // 30 + 1))
+        for ul_num in range(1, 4):
+            for li_num in range(1, 11):
+                cnt += 1
+                if cnt >= MAX_REQUESTS:  # 安全考慮
+                    result.append('Too many requests > {}'.format(MAX_REQUESTS))
+                    return result
+                header = soup.select(TITLE_SELECTOR.format(ul_num, li_num))
+                link, title = header[0].attrs['href'], header[0].contents[0].contents[0]
+
+                time_box = soup.select(CONTENT_DATE_SELECTOR.format(ul_num, li_num))
+                content_date = time_box[0].contents[0]
+                
+                print('\r                                                                                 \r', end='')
+                print('   [{}] Checking - ({}) {}'.format(cnt, content_date, title), end='', flush=True)
+
+                if is_target_date(TARGET_DATE, content_date):
+                    stage = 2
+                    if any(fi_word in title for fi_word in FI_WORDS):
+                        time.sleep(random.randint(1, 2))
+                        html = lxml.html.fromstring(session.get(BASE_URL+link, headers=HEADERS).text)
+                        hr_article = html.xpath(HR_CONTENT_XPATH)[0].text_content()
+                        result.append('[{}]({}) ({}) > {}'.format(title, BASE_URL + link, content_date, hr_article))
+
+                else:
+                    if stage == 2:
+                        print('')
+                        return result
+    print('')
+    return result
