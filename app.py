bf4e7b5b13bca35f8b193944bad176e0a4195119
makoto@Makotos-MacBook.local <makoto@Makotos-MacBook.local.local>
2024-03-31 23:46:47 +0900

    2024年3月のWebサイトの構造変化への対応

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request
-from utils.crawl_hr_news import crawl_hr_news
+from utils.crawl_hr_news import CrawlHrNewsInNikkei, CrawlHrNewsInNikkeiError
 from datetime import datetime, timedelta
 import os
+import traceback
 
 DEFAULT_TARGET_DATE: str = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
 DEFAULT_FI_WORDS = ['銀行', 'バンク', '金庫', '信金', '信用', '組合', 'ファイナンシャル', 'フィナンシャル', 'ファイナンス', '証券', '證券', '保険', '損保', '生命', '生保', '金融庁']
@@ -13,11 +14,16 @@ app = Flask(__name__)
 
 @app.route('/', methods = ["GET" , "POST"]) #2 GETとPOSTをリクエストできるようにする
 def index():
-    if request.method == 'POST': #3POSTの処理
+    if request.method == 'POST': #3 POSTの処理
+        base_url = 'https://www.nikkei.com'
         target_date = request.form['target_date']
         fi_words = request.form['fi_words']
-        result = crawl_hr_news(str(int(target_date.split('-')[-1])), fi_words.split(','))
-        return render_template('index.html', target_date=target_date, fi_words=fi_words, result='\n'.join(result), result_count=len(result) )
+        crawler = CrawlHrNewsInNikkei(base_url, fi_words.split(','), target_date)
+        try:
+            result_count, result = crawler.start_crawling()
+        except CrawlHrNewsInNikkeiError as e:
+            result_count, result = 0, "!"*50 + "\nエラーが発生しました\n" + "!"*50 + "\n\n" + traceback.format_exc()
+        return render_template('index.html', target_date=target_date, fi_words=fi_words, result=result, result_count=result_count)
     return render_template('index.html', target_date=DEFAULT_TARGET_DATE, fi_words=','.join(DEFAULT_FI_WORDS)) #6 GETの処理
 
 if __name__ == '__main__':
--- utils/crawl_hr_news.py
+++ utils/crawl_hr_news.py
@@ -1,66 +1,162 @@
-
 import requests
-from bs4 import BeautifulSoup
-import lxml.html
-import time
+from bs4 import BeautifulSoup, Tag
 import datetime
+import time
 import random
+import re
+from zoneinfo import ZoneInfo
+from typing import Literal, Union
 
-def is_target_date(target_date: str, content_date: str):
-    idx_hi: int = content_date.find('日')
-    return idx_hi >= 0 and target_date == content_date[0:idx_hi]
+class CrawlHrNewsInNikkeiError(Exception):
+    pass
 
-# def is_GRCS_related(article: str):
-#     return any(grcs_word in article for grcs_word in GRCS_WORDS)
+class CrawlHrNewsInNikkei():
+    def __init__(self, base_url: str, fi_words: list[str], target_date: str, max_requests: int = 1000, max_pages: int = 100, max_duration: Union[int,float] = 300):
+        '''
+        max_requests: Webサイトに負荷をかけすぎないように最大のリクエスト数を定める。デフォルトは1000。
+        max_pages: page番号の最大値を定める。デフォルトは100(Webサイトの仕様制約)。
+        max_duration: 処理の最大時間秒数。この時間を過ぎると処理結果を途中で返す。デフォルトは5分(300秒)。
+        '''
+        self.HTTP_HEADERS: dict[str, str] = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Encoding": "gzip" 
+        }
+        self.BASE_URL: str = base_url
+        self.FI_WORDS: list[str] = fi_words
+        self.TARGET_DATE: datetime.date = datetime.datetime.strptime(target_date + '+0900', r'%Y-%m-%d%z').date()
+        self.MAX_REQUESTS: int = max_requests
+        self.MAX_PAGES: int = max_pages
+        self.MAX_DURATION: float = float(max_duration) 
+        self.ZONE_INFO: ZoneInfo = ZoneInfo("Asia/Tokyo")
+        self.result: list[str] = []
+        self.starting_page_no: int = 1
+        self.request_cnt: int = 0
+        self.crawling_status: int = 0  # 0: 実行中以外, 1: 実行を継続, 2: 実行を終わる
+        
+    def _compare_to_target_date(self, hatsurei_date: datetime.datetime) -> Literal['earlier', 'later', 'same']:
+        if hatsurei_date.date() < self.TARGET_DATE:
+            return "earlier"
+        elif hatsurei_date.date() > self.TARGET_DATE:
+            return 'later'
+        else:
+            return 'same'
 
-BASE_URL: str = 'https://www.nikkei.com'
-MAX_REQUESTS = 1000
+    def _is_financial_institution(self, title: str):
+        return any(fi_word in title for fi_word in self.FI_WORDS)
 
-TITLE_SELECTOR = '#CONTENTS_MAIN > section > ul:nth-of-type({}) > li:nth-of-type({}) > div > div.col.headline > h3 > a'
-    ##CONTENTS_MAIN > section > ul:nth-child(2) > li:nth-child(1) > div > div.col.headline > h3 > a
-CONTENT_DATE_SELECTOR = '#CONTENTS_MAIN > section > ul:nth-of-type({}) > li:nth-of-type({}) > div > div.col.time > p'
-    ##CONTENTS_MAIN > section > ul:nth-child(2) > li:nth-child(1) > div > div.col.time > p
-HR_CONTENT_XPATH = '/html/body/div[5]/main/article/section[1]'
+    def _scrape_jinji_cards(self, session: requests.Session, page: int) -> list[Tag]:
+        '''
+        "https://www.nikkei.com/news/jinji/hatsurei/?page={page}"のHTMLを取得し、jinjiCard部分を抜き出す
 
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-}
-session = requests.Session()
+        '''
+        self.request_cnt += 1
+        try:
+            soup = BeautifulSoup(session.get(self.BASE_URL+'/news/jinji/hatsurei/?page='+str(page), headers=self.HTTP_HEADERS).text, 'html.parser')
+        except Exception as ex:
+            raise CrawlHrNewsInNikkeiError(f"{self.BASE_URL+'/news/jinji/hatsurei/?page='+str(page)}へのリクエストまたはパースに失敗しました。")
+        
+        return soup.find_all('article', attrs={'class': re.compile(r'^jinjiCard[\w]+$')})
 
-def crawl_hr_news(TARGET_DATE: str, FI_WORDS: list) -> list:
-    stage = 1  # stage: 1-TARGET_DATEに達していない状態, 2-達した状態
-    result = []
-    cnt = 0  # cnt: ループの回数を記録する
-    for bn in range(1, 1982, 30):  # ページのURLが /?bn=31, /?bn=61のように30刻みとなっていることに対応
-        time.sleep(random.randint(1, 2))
-        soup = BeautifulSoup(session.get(BASE_URL+'/news/jinji/hatsurei/?bn='+str(bn), headers=HEADERS).text, features="lxml")
-        for ul_num in range(1, 4):
-            for li_num in range(1, 11):
-                cnt += 1
-                if cnt >= MAX_REQUESTS:  # 安全考慮
-                    result.append('Too many requests > {}'.format(MAX_REQUESTS))
-                    return result
-                header = soup.select(TITLE_SELECTOR.format(ul_num, li_num))
-                link, title = header[0].attrs['href'], header[0].contents[0].contents[0]
+    def _scrape_jinji_article(self, session: requests.Session, href: str) -> list[Tag]:
+        '''
+        "https://www.nikkei.com/article/XXXXXXXXXX/"のHTMLを取得し、paragraph部分を抜き出す
 
-                time_box = soup.select(CONTENT_DATE_SELECTOR.format(ul_num, li_num))
-                content_date = time_box[0].contents[0]
-                
-                print('\r                                                                                 \r', end='')
-                print('   [{}] Checking - ({}) {}'.format(cnt, content_date, title), end='', flush=True)
+        '''
+        self.request_cnt += 1
+        try:
+            soup = BeautifulSoup(session.get(self.BASE_URL + href, headers=self.HTTP_HEADERS).text, 'html.parser')
+        except Exception as ex:
+            raise CrawlHrNewsInNikkeiError(f"{self.BASE_URL + href}へのリクエストまたはパースに失敗しました。")
+        return soup.find_all('p', {'class': re.compile(r'^paragraph_[\w]+$')})
 
-                if is_target_date(TARGET_DATE, content_date):
-                    stage = 2
-                    if any(fi_word in title for fi_word in FI_WORDS):
-                        time.sleep(random.randint(1, 2))
-                        html = lxml.html.fromstring(session.get(BASE_URL+link, headers=HEADERS).text)
-                        hr_article = html.xpath(HR_CONTENT_XPATH)[0].text_content()
-                        result.append('[{}]({}) ({}) > {}'.format(title, BASE_URL + link, content_date, hr_article))
+    def _get_starting_page_no(self, session: requests.Session) -> int:
+        '''
+        Crawlingをスタートするページを見つける
+        - 1ページ目がスタート対象であるかをチェック。対象であれば1を返す
+        - 指定した日付が１ページ目にのっている最新の日付よりも新しい場合は1を返す
+        - 違う場合は2分探索により見つける
+        '''
+        print(f"Checking page-{1}")
+        jinji_cards = self._scrape_jinji_cards(session, 1)
+        for jinji_card in jinji_cards:
+            el_time = jinji_card.find('time')
+            el_time_datetime = datetime.datetime.fromisoformat(el_time.get('datetime').replace('Z', '+00:00')).astimezone(self.ZONE_INFO)
+            if el_time_datetime.date() <= self.TARGET_DATE:
+                return 1
 
-                else:
-                    if stage == 2:
-                        print('')
-                        return result
-    print('')
-    return result
+        def solve(n):
+            print(f"Checking page-{n}")
+            time.sleep(random.random())
+            jinji_cards = self._scrape_jinji_cards(session, n)
+            if jinji_cards:
+                jinji_card = jinji_cards[-1]
+                el_time = jinji_card.find('time')
+                el_time_datetime = datetime.datetime.fromisoformat(el_time.get('datetime').replace('Z', '+00:00')).astimezone(self.ZONE_INFO)
+                return el_time_datetime.date() <= self.TARGET_DATE
+            else:
+                return True
+
+        # [ok, ng) - Maximum
+        # (ng, ok] - Minimum
+        # ok が 最終的な答え
+        ok = self.MAX_PAGES
+        ng = 0
+        while abs(ok - ng) > 1:
+            mid = (ok + ng) // 2
+            if solve(mid):
+                ok = mid
+            else:
+                ng = mid
+        return ok
+
+    def start_crawling(self) -> tuple[int, str]:
+        start_datetime = datetime.datetime.now(self.ZONE_INFO)
+        self.crawling_status = 1
+        self.result.clear()
+        with requests.session() as session:
+            self.starting_page_no = self._get_starting_page_no(session)
+            for page in range(self.starting_page_no, self.MAX_PAGES + 1):
+                print(f'Starting page-{page}', flush=True)
+                time.sleep(2 * random.random())
+                jinji_cards = self._scrape_jinji_cards(session, page)
+                for jinji_card in jinji_cards:
+                    el_time = jinji_card.find('time')
+                    el_time_datetime = datetime.datetime.fromisoformat(el_time.get('datetime').replace('Z', '+00:00')).astimezone(self.ZONE_INFO)
+                    el_a = jinji_card.find('a', attrs={'class': re.compile(r'^fauxBlockLink_[\w]+$')})
+                    el_a_text = el_a.text
+                    el_a_href = el_a.get('href')
+                    date_comparison_result = self._compare_to_target_date(el_time_datetime)
+                    if date_comparison_result == 'same':  # targe_dateと同じ日付であればリンク先をスクレイピングする
+                        if self._is_financial_institution(el_a_text):
+                            self.result.append(f'**[{el_a_text}]({self.BASE_URL + el_a_href}) ({el_time_datetime.strftime(r"%m/%d %H:%M")})**')
+                            time.sleep(2 * random.random())
+                            paragraphs = self._scrape_jinji_article(session, el_a_href)
+                            for paragraph in paragraphs:
+                                self.result.extend(paragraph.text.split('▽'))
+                    elif date_comparison_result == 'later':  # target_dateよりも後の日付の場合、何も処理は行わず次の要素に進む
+                        continue
+                    elif date_comparison_result == 'earlier':  # target_dateよりも前の日付の場合、crawlingを終了する
+                        self.crawling_status = 2
+                        break
+                    
+                    # MAX_REQUESTSを超過した場合
+                    if self.request_cnt > self.MAX_REQUESTS:
+                        self.crawling_status = 2
+                        self.result = ["!" * 50, f"リクエスト数の最大値 {self.MAX_REQUESTS} を超過したため処理を途中で中断しました", "!" * 50] + self.result
+                        break
+
+                    # MAX_DURATIONを超過した場合
+                    if (datetime.datetime.now(self.ZONE_INFO) - start_datetime).total_seconds() > self.MAX_DURATION:
+                        self.crawling_status = 2
+                        self.result = ["!" * 50, f"処理時間の最大秒数 {self.MAX_DURATION} を超過したため処理を途中で中断しました", "!" * 50] + self.result
+                        break
+
+                if self.crawling_status == 2:
+                    break
+
+        self.crawling_status = 0
+        if self.result:
+            return len(self.result), '\n'.join(self.result)
+        else:
+            return 0, "結果は0件でした"
